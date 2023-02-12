f3863e218d371ce856b8cc9b15d7a5fcc21492f2
Podavalenko <deidra2@yandex.ru>
2023-02-12 12:43:46 +0300

    Initial commit

---
+++ app/src/main/java/ru/netology/hwcustomview/MainActivity.kt
@@ -0,0 +1,19 @@
+package ru.netology.hwcustomview
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<StatsView>(R.id.stats).data = listOf(
+            500F,
+            500F,
+            500F,
+            500F
+        )
+    }
+}
